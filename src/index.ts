8af3b03f8381e7cfe344d7cc31900cc298bcebba
Rodrigo Bustamante <rbustamante@acid.cl>
2020-01-27 23:12:19 -0300

    Assing PORT for graphql by env variable, if isn't assigned, use port 4000 by default

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,12 @@
 import dotenv from 'dotenv';
-import express from 'express';
-import { ApolloServer, gql } from 'apollo-server';
-import communes from './routes/communes';
-import properties from './routes/properties';
+import { ApolloServer } from 'apollo-server';
 import propertiesSchema from './schemas/property';
 import propertiesResolver from './resolvers/property';
 
-const server = new ApolloServer({ typeDefs: propertiesSchema, resolvers: propertiesResolver });
-
-// The `listen` method launches a web server.
-server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
-});
-
 dotenv.config();
 
-// const { PORT } = process.env;
-// const app = express();
+const server = new ApolloServer({ typeDefs: propertiesSchema, resolvers: propertiesResolver, });
 
-// (async (): Promise<void> => {
-//   app.use(express.json());
-
-//   app.use('/communes', communes);
-//   app.use('/properties', properties);
-
-//   app.listen(PORT, () => console.log(`Listen port ${PORT}!`))
-// })();
+server.listen({port: process.env.PORT || 4000}).then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
